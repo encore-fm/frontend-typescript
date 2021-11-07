3c91271a5626ba6b2efa36ec060a65247d863f4b
omareldeeb <omareldeeb00@live.com>
2021-11-07 19:30:39 +0100

    Implement NoBorder on last PlaylistElement

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Playlist from './components/Playlist'
+
+const playlist: Song[] = [
+    {
+        id: "123",
+        name: "song name",
+        artists: ["omar, anton"],
+        suggestedBy: "anton",
+        album: "rick roll",
+        imageURL: "https://s.yimg.com/ny/api/res/1.2/M142gEoUDb8sPr_LZCGNpw--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQxMw--/https://s.yimg.com/uu/api/res/1.2/_fPK2Z4nDW.dPiYeimLx8w--~B/aD00MDA7dz02MjA7YXBwaWQ9eXRhY2h5b24-/https://media.zenfs.com/en/thewrap.com/e3a98e6aea832ab72f5a406c2faaa419"
+    },
+    {
+        id: "321",
+        name: "name of song",
+        artists: ["anton, omar"],
+        suggestedBy: "omar",
+        album: "roll rick",
+        imageURL: "https://static.giga.de/wp-content/uploads/2016/02/whenever-you-need-somebody-rcm797x0.jpg"
+    }
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Playlist
+        playlist={playlist}
+      />
     </div>
   );
 }
--- src/types/Song.d.ts
+++ src/types/Song.d.ts
@@ -1,7 +1,8 @@
 type Song = {
-    id: String
-    name: String,
-    artist: String,
-    album: String,
-    imageURL: String
+    id: string
+    name: string
+    artists: string[]
+    album: string
+    suggestedBy: string
+    imageURL: string
 }
