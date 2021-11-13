c0bbcff147ea0be4b78f5e8bcca991d019353c18
omareldeeb <omareldeeb00@live.com>
2021-11-13 17:31:36 +0100

    Implement TrackSearchResultsElement

--- src/components/SongInfo.tsx
+++ src/components/SongInfo.tsx
@@ -1,22 +1,34 @@
 import React from 'react'
+import { prettyTime } from '../util/Formatter'
 import './SongInfo.scss'
 
 type SongInfoProps = {
   songName: string
   album: string
   artists: string[]
-  suggestedBy: string
+  suggestedBy?: string
+  durationMs?: number
 }
 
-const SongInfo = ({ songName, album, artists, suggestedBy }: SongInfoProps) => {
+const SongInfo = ({ songName, album, artists, suggestedBy, durationMs }: SongInfoProps) => {
   return (
     <div className="SongInfo">
       <div className="SongInfo_songName">{songName}</div>
       <div className="SongInfo_album">{album}</div>
       <div className="SongInfo_artists">{artists.join(', ')}</div>
-      <div className="SongInfo_suggestedBy">
+
+      {suggestedBy && (
+        <div className="SongInfo_suggestedBy">
         suggested by <span className="highlight">{suggestedBy}</span>
-      </div>
+        </div>
+      )}
+
+      {durationMs && (
+        <div className="SongInfo_duration">
+          {prettyTime(durationMs)}
+        </div>
+      )}
+      
     </div>
   )
 }
