782e08aa1e794594def3631d0d321551f7081e9c
Omar Eldeeb <40169383+omareldeeb@users.noreply.github.com>
2021-11-14 15:30:47 +0100

    Feature/track search results (#12)
    * Make size and highlight optional in IconProps
    
    * Move Formatter to utils
    
    * Implement TrackSearchResultsElement
    
    * Implement TrackSearchResults
    
    * Remove length check before map

---
+++ src/components/SuggestButton.tsx
@@ -0,0 +1,15 @@
+import IconPlus from "./icons/IconPlus";
+
+type SuggestButtonProps = {
+    onClick: () => void
+}
+
+const SuggestButton = ({onClick}: SuggestButtonProps) => {
+    return (
+        <div className="SuggestButton">
+            <IconPlus onClick={onClick}/>
+        </div>
+    )
+}
+
+export default SuggestButton;
